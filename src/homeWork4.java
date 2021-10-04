6798e37d0d03511e80543052fe5e02dbb1114144
efire199 <efire199@gmail.com>
2021-10-04 18:38:20 +0300

    second commit

---
+++ src/homeWork4.java
@@ -0,0 +1,25 @@
+public class homeWork4 {
+    public static void main(String[] args) {
+
+        String[] strings = new String[] {
+                "Первое",
+                "Второе",
+                "Третье",
+                "Четвертое",
+                "Пятое",
+                "Шестое",
+                "Седьмое",
+                "Восьмое",
+                "Девятое",
+                "Десятое",
+                "Второе",
+                "Четвертое",
+                "Пятое",
+                "Второе",
+                "Десятое"
+        };
+        countString.printString(strings);
+
+
+    }
+}
